ec07c9b6c198057fc85cacf091bedbcddd4332cf
Chana <twszak01@gmail.com>
2024-04-18 01:52:05 +0900

    feat : make selected card into main card ing : mark selected card in card selector

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
+"use strict"
 console.log("Javascript Loaded");
+
+// 클릭한 메인 카드 정의
+let selectedMainCard = null;
+
+// 카드 이미지 경로 정의
+const CARD_IMAGE_ROOT = '../resources/cards/';
+
+// 카드 셀렉터 정의
+const $CARD_SELECTOR = document.querySelector('.card-selector-container');
 
 /*
 이미지 정의
@@ -12,17 +22,36 @@ const cardNumbers = [
   "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"
 ];
 
-for (i = 0; i < cardSuits.length; i++) {
-  for (j = 0; j < cardNumbers.length; j++) {
+for (let i = 0; i < cardSuits.length; i++) {
+  for (let j = 0; j < cardNumbers.length; j++) {
     imageStrings[i * 13 + j] = cardNumbers[j] + cardSuits[i];
   }
 }
 
 /*
+카드 셀렉터에서 카드 골랐을 경우 메인 카드에 넣기
+*/
+let cardSelect = function (e) {
+  /*
+  이미 선택되어있는 카드일 경우 선택할 수 없음
+  */
+
+  let cardID = e.target.id;
+  console.log(e.target);
+  if (!selectedMainCard) return; // 혹시 메인카드가 골라져있지 않다면 Error.
+
+  let imagePath = `${CARD_IMAGE_ROOT}${cardID}${png}`;
+  selectedMainCard.style.backgroundImage = `url(${imagePath})`;
+  selectedMainCard.style.backgroundSize = 'cover';
+
+  e.target.style.filter = 'brightness(70%)';
+
+  $CARD_SELECTOR.style.visibility = 'hidden';
+}
+
+/*
 카드 셀렉터 초기화
 */
-$cardSelector = document.querySelector('.card-selector-container');
-
 let initCardSelector = function ($cardSelector) {
   for (let i = 0; i < 4; i++) {
     let line = document.createElement('div');
@@ -33,42 +62,48 @@ let initCardSelector = function ($cardSelector) {
       card.classList.add('card-selector-card');
       card.id = `${cardNumbers[j]}${cardSuits[i]}`;
 
-      let imagePath = `../resources/cards/${cardNumbers[j]}${cardSuits[i]}` + png;
+      let imagePath = `${CARD_IMAGE_ROOT}${cardNumbers[j]}${cardSuits[i]}${png}`;
       card.style.backgroundImage = `url(${imagePath})`;
       card.style.backgroundSize = 'cover';
+
+      // 카드 셀렉터 내부의 카드를 클릭했을 때 메인 카드에 입력
+      card.addEventListener('click', cardSelect);
 
       line.appendChild(card);
     }
     $cardSelector.appendChild(line);
   }
 }
-initCardSelector($cardSelector);
+initCardSelector($CARD_SELECTOR);
 
 /*
 카드 셀렉터 보여주기
 */
 let showCardSelector = function (e) {
+  let mainCardID = e.target.id;
+  selectedMainCard = document.querySelector('#' + mainCardID);
+  console.log(e.target.id);
   const x = e.clientX;
   const y = e.clientY;
 
   const viewportWidth = window.innerWidth;
   const viewportHeight = window.innerHeight;
 
-  $cardSelector.style.top = y + 'px';
+  $CARD_SELECTOR.style.top = y + 'px';
 
-  if (x + $cardSelector.offsetWidth > viewportWidth) {
-    $cardSelector.style.left = (viewportWidth - $cardSelector.offsetWidth) + 'px';
+  if (x + $CARD_SELECTOR.offsetWidth > viewportWidth) {
+    $CARD_SELECTOR.style.left = (viewportWidth - $CARD_SELECTOR.offsetWidth) + 'px';
   } else {
-    $cardSelector.style.left = x + 'px';
+    $CARD_SELECTOR.style.left = x + 'px';
   }
 
-  $cardSelector.style.visibility = 'hidden';
-  $cardSelector.style.visibility = 'visible';
+  $CARD_SELECTOR.style.visibility = 'hidden';
+  $CARD_SELECTOR.style.visibility = 'visible';
 };
 
 let $$cards = document.querySelectorAll('.main-card');
 
-for (i = 0; i < $$cards.length; i++) {
+for (let i = 0; i < $$cards.length; i++) {
   $$cards[i].addEventListener('click', showCardSelector);
 }
 
@@ -81,7 +116,7 @@ document.body.addEventListener('click', function (e) {
   const isCardSelector = e.target.closest('.card-selector-container');
 
   if (!isMainCard && !isCardSelector) {
-    $cardSelector.style.visibility = 'hidden';
+    $CARD_SELECTOR.style.visibility = 'hidden';
   }
 });
 
