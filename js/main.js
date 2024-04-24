7b1addaa0a5b00e000bbb64874e09d3230d505e9
Chana <twszak01@gmail.com>
2024-04-24 21:00:49 +0900

    feat : blcoking selection for card already selected refactor : modified id for main-card element in HTML

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const CARD_IMAGE_ROOT = '../resources/cards/';
 const $CARD_SELECTOR = document.querySelector('.card-selector-container');
 
 /*
-이미지 정의
+이미지 정의 및 변수 초기화
 */
 let imageStrings = [];
 const cardSuits = [
@@ -22,27 +22,48 @@ const cardNumbers = [
   "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"
 ];
 
+// 선택된 카드들 확인
+const isCardSelected = [];
+// 메인 카드에 선택된 카드들
+const cardInMainCard = [];
+
 for (let i = 0; i < cardSuits.length; i++) {
+  isCardSelected[i] = [];
   for (let j = 0; j < cardNumbers.length; j++) {
+    isCardSelected[i][j] = false;
     imageStrings[i * 13 + j] = cardNumbers[j] + cardSuits[i];
   }
 }
 
 /*
-카드 셀렉터에서 카드 골랐을 경우 메인 카드에 넣기
+카드 셀렉터에서 카드 고르기
 */
 let cardSelect = function (e) {
   /*
   이미 선택되어있는 카드일 경우 선택할 수 없음
   */
+  let numberIndex = e.target.numberIndex;
+  let suitIndex = e.target.suitIndex;
 
+  if (isCardSelected[suitIndex][numberIndex]) {
+    return;
+  }
+
+  /*
+  카드 선택 작업
+  */
   let cardID = e.target.id;
   console.log(e.target);
-  if (!selectedMainCard) return; // 혹시 메인카드가 골라져있지 않다면 Error.
+  if (!selectedMainCard) return; // 변경할 메인카드가 골라져있지 않다면 Error.
 
   let imagePath = `${CARD_IMAGE_ROOT}${cardID}${png}`;
   selectedMainCard.style.backgroundImage = `url(${imagePath})`;
   selectedMainCard.style.backgroundSize = 'cover';
+  isCardSelected[suitIndex][numberIndex] = true;
+
+  /*
+
+  */
 
   e.target.style.filter = 'brightness(70%)';
 
@@ -62,6 +83,10 @@ let initCardSelector = function ($cardSelector) {
       card.classList.add('card-selector-card');
       card.id = `${cardNumbers[j]}${cardSuits[i]}`;
 
+      // 해당 카드의 숫자와 문양 인덱스 속성 추가
+      card.numberIndex = j;
+      card.suitIndex = i;
+
       let imagePath = `${CARD_IMAGE_ROOT}${cardNumbers[j]}${cardSuits[i]}${png}`;
       card.style.backgroundImage = `url(${imagePath})`;
       card.style.backgroundSize = 'cover';
@@ -77,10 +102,13 @@ let initCardSelector = function ($cardSelector) {
 initCardSelector($CARD_SELECTOR);
 
 /*
+메인 카드 클릭 시
 카드 셀렉터 보여주기
 */
 let showCardSelector = function (e) {
   let mainCardID = e.target.id;
+
+  // 클릭한 메인카드
   selectedMainCard = document.querySelector('#' + mainCardID);
   console.log(e.target.id);
   const x = e.clientX;
