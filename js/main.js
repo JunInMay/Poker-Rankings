41ee83ec623e9525f6ba5e00c493f8a040b92a29
Chana <twszak01@gmail.com>
2024-04-25 15:34:56 +0900

    feat : print card rankings

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ let $selectedMainCard = null;
 let selectedMainCardIndex = null;
 
 // 메인 카드에 선택된 카드들이 각각 카드 셀렉터에 어느 위치에 저장되어 있는지 확인하기 위한 인덱스
+// [문양, 숫자] 형식으로 저장
 const selectedCardInMainCard = [];
 
 // 카드 이미지 경로 정의
@@ -25,14 +26,114 @@ const png = ".png";
 const CARD_NUMBERS = [
   "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"
 ];
-
-// 카드 셀렉터 내부의 카드
-const cardInCardSelector = [];
-
 for (let i = 0; i < CARD_SUITS.length; i++) {
   for (let j = 0; j < CARD_NUMBERS.length; j++) {
     imageStrings[i * 13 + j] = CARD_NUMBERS[j] + CARD_SUITS[i];
   }
+}
+
+// 카드 셀렉터 내부의 카드
+const cardInCardSelector = [];
+
+
+/*
+족보 계산
+*/
+const $RANKINGS_TEXT_CONTAINER_TEXT = document.querySelector('.rankings-text-container-text');
+
+let showRankings = function () {
+  $RANKINGS_TEXT_CONTAINER_TEXT.textContent = "Highcard";
+  // 어떤 문양이 몇 개고, 어떤 숫자가 몇 개인지 담을 배열 초기화
+  let suitsCount = [];
+  let numbersCount = [];
+  for (let i = 0; i < CARD_SUITS.length; i++) {
+    suitsCount[i] = 0;
+  }
+  for (let i = 0; i < CARD_NUMBERS.length; i++) {
+    numbersCount[i] = 0;
+  }
+
+  // 어떤 카드가 골라졌는지 확인하고, 개수를 체크
+  for (let i = 0; i < 5; i++) {
+    if (!selectedCardInMainCard[i]) continue;
+
+    let suitIndex = selectedCardInMainCard[i][0];
+    let numberIndex = selectedCardInMainCard[i][1];
+
+    suitsCount[suitIndex] += 1;
+    numbersCount[numberIndex] += 1;
+  }
+
+  // flush check
+  let isFlush = false;
+  for (let i = 0; i < CARD_SUITS.length; i++) {
+    if (suitsCount[i] == 5) isFlush = true;
+  }
+
+  // pair check
+  let isPair = 0;
+  let isThree = 0;
+  let isFour = 0;
+  for (let i = 0; i < CARD_NUMBERS.length; i++) {
+    if (numbersCount[i] == 2) {
+      isPair += 1;
+    } else if (numbersCount[i] == 3) {
+      isThree += 1;
+    } else if (numbersCount[i] == 4) {
+      isFour += 1;
+    }
+  }
+
+  // straight check
+  let startIndex = null;
+  let consecutiveCount = 0;
+  for (let i = -1; i < CARD_NUMBERS.length; i++) {
+    // alternative index
+    let ai = (i + CARD_NUMBERS.length) % CARD_NUMBERS.length;
+    if (startIndex == null && numbersCount[ai]) {
+      startIndex = ai;
+      consecutiveCount = 1;
+      continue;
+    } else if (startIndex != null && !numbersCount[ai]) {
+      startIndex = null;
+      consecutiveCount = 0;
+      break;
+    }
+    consecutiveCount += 1;
+
+    if (consecutiveCount == 5) break;
+  }
+  
+  let isStraight = false;
+  let isMountain = false;
+  let isBackstraight = false;
+  if (consecutiveCount == 5) {
+    isStraight = true;
+    if (startIndex == CARD_NUMBERS.length - 1) isBackstraight = true;
+    if (startIndex == CARD_NUMBERS.lenght - 6) isMountain = true;
+  }
+
+  console.log(`stats
+    isPair = ${isPair}
+    isThree = ${isThree}
+    isFour = ${isFour}
+    isStraight = ${isStraight}
+    isFlush = ${isFlush}
+    suitsCount = ${suitsCount}
+    numbersCount = ${numbersCount}
+  `);
+
+  if (isFlush && isStraight) $RANKINGS_TEXT_CONTAINER_TEXT.textContent = "Staright Flush";
+  else if (isFour) $RANKINGS_TEXT_CONTAINER_TEXT.textContent = "Four of a kind";
+  else if (isFlush) $RANKINGS_TEXT_CONTAINER_TEXT.textContent = "Flush";
+  else if (isThree && isPair) $RANKINGS_TEXT_CONTAINER_TEXT.textContent = "Full House";
+  else if (isStraight) $RANKINGS_TEXT_CONTAINER_TEXT.textContent = "Straight";
+  else if (isThree) $RANKINGS_TEXT_CONTAINER_TEXT.textContent = "Three of a kind";
+  else if (isPair == 2) $RANKINGS_TEXT_CONTAINER_TEXT.textContent = "Two pair";
+  else if (isPair == 1) $RANKINGS_TEXT_CONTAINER_TEXT.textContent = "One pair";
+  /*
+  마운틴, 백스트레이트도 있긴 한데 서양 정식 족보는 아니라 집계만 하고 표기에선 제외.
+  */
 }
 
 /*
@@ -45,8 +146,6 @@ let cardSelect = function (e) {
   let suitIndex = e.target.suitIndex;
   let numberIndex = e.target.numberIndex;
 
-  console.log(cardInCardSelector[suitIndex][numberIndex]);
-
   if (cardInCardSelector[suitIndex][numberIndex].selected) {
     return;
   }
@@ -55,7 +154,6 @@ let cardSelect = function (e) {
   카드 선택 작업
   */
   let cardID = e.target.id;
-  console.log(e.target);
   if (!$selectedMainCard || selectedMainCardIndex == null) return; // 변경할 메인카드가 골라져있지 않다면 Error.
 
   let imagePath = `${CARD_IMAGE_ROOT}${cardID}${png}`;
@@ -76,9 +174,13 @@ let cardSelect = function (e) {
     cardPreSelected.style.filter = 'brightness(100%)';
   }
   selectedCardInMainCard[selectedMainCardIndex] = [suitIndex, numberIndex];
-  console.log(selectedCardInMainCard)
 
   selectedMainCardIndex = null;
+
+  /*
+  족보 보이기
+  */
+  showRankings();
 }
 
 /*
@@ -98,7 +200,7 @@ let initCardSelector = function ($cardSelector) {
       // 해당 카드의 숫자와 문양 인덱스 속성 추가
       card.numberIndex = j;
       card.suitIndex = i;
-      
+
       // 선택 여부 속성 추가
       card.selected = false;
 
